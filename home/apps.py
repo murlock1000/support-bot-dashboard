4031fa0c9d321785829fbe59e4a6527c66b6cbf0
murlock1000 <pauliusjankauskas10@gmail.com>
2023-09-18 18:49:11 +0300

    Display open ticket data

--- home/apps.py
+++ home/apps.py
@@ -1,27 +1,23 @@
-import logging
 from django.apps import AppConfig
-
-from middleman.models.Repositories.TicketRepository import TicketRepository
 from support_lib.config import Config
 from support_lib.storage import Storage
 from middleman.models.Repositories.Repositories import Repositories
 from django.conf import settings
+
+store: Storage = None
+config: Config = None
 
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
     
     def ready(self):
-        self.config = Config(settings.SUPPORT_BOT_CONFIG_PATH)
+        global config, store
+        config = Config(settings.SUPPORT_BOT_CONFIG_PATH)
         
         # Configure the database
-        self.store = Storage(self.config.database)
+        store = Storage(config.database)
 
         # Initialise global model repositories:
-        repositories = Repositories(self.store)
-        self.store.set_repositories(repositories)
-        
-        ticket_rep: TicketRepository = self.store.repositories.ticketRep
-        
-        tickets = ticket_rep.get_open_tickets()
-        logging.warning("Tickets: %s", tickets)
+        repositories = Repositories(store)
+        store.set_repositories(repositories)
--- home/views.py
+++ home/views.py
@@ -1,9 +1,29 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.apps import store
+from middleman.models.Repositories.TicketRepository import TicketRepository
 
 # Create your views here.
 
 def index(request):
 
-    # Page from the theme 
-    return render(request, 'pages/index.html')
+    ticket_rep: TicketRepository = store.repositories.ticketRep
+        
+    open_tickets = ticket_rep.get_open_tickets()
+    
+    ticket_meta = []
+    for ticket in open_tickets:
+        ticket_id = ticket['id']
+        ticket_meta.append({
+                'meta': ticket,
+                'staff': ticket_rep.get_assigned_staff(ticket_id),
+            }
+        )
+    
+    context = {
+        'open_tickets': ticket_meta,
+    }
+
+    # Page from the theme
+    return render(request, 'pages/index.html', context)
